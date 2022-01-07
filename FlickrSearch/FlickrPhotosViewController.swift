98e6472cf5cdfe76b35dd5ebafd7ab54b396fb45
yanghojoon <yanghojoon8487@gmail.com>
2022-01-07 16:34:28 +0900

    feat: 섹션의 크기 및 사진을 찾는 메서드 추가

---
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,85 @@
+/// Copyright (c) 2022 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+
+
+
+class FlickrPhotosViewController: UICollectionViewController {
+  private let reuseIdentifier = "FlickrCell"
+  private let sectionInsets = UIEdgeInsets(
+    top: 50.0,
+    left: 20.0,
+    bottom: 50.0,
+    right: 20.0
+  )
+  private var searches: [FlickrSearchResults] = []
+  private let flickr = Flickr()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Register cell classes
+    self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  // MARK: UICollectionViewDataSource
+  
+  override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 0
+  }
+  
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of items
+    return 0
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+    // Configure the cell
+    
+    return cell
+  }
+  
+  // MARK: UICollectionViewDelegate
+
+}
+
+private extension FlickrPhotosViewController {
+  func photo(for indexPath: IndexPath) -> FlickrPhoto {
+    return searches[indexPath.section].searchResults[indexPath.row]
+  }
+}
